5d2b2aee1b448f5914ff4518f0b31c7c8c213c18
Anuran Pradhan <anuranpradhan002@gmail.com>
2026-02-23 10:47:55 +0530

    Fix web layout: explicit container element and fixed tab bar width

--- app/+html.tsx
+++ app/+html.tsx
@@ -2,15 +2,15 @@ import { ScrollViewStyleReset } from 'expo-router/html';
 import type { PropsWithChildren } from 'react';
 
 export default function Root({ children }: PropsWithChildren) {
-    return (
-        <html lang="en">
-            <head>
-                <meta charSet="utf-8" />
-                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-                <ScrollViewStyleReset />
-                <style dangerouslySetInnerHTML={{
-                    __html: `
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <ScrollViewStyleReset />
+        <style dangerouslySetInnerHTML={{
+          __html: `
           @media (min-width: 600px) {
             body {
               background-color: #f3f4f6;
@@ -29,8 +29,29 @@ export default function Root({ children }: PropsWithChildren) {
             }
           }
         `}} />
-            </head>
-            <body>{children}</body>
-        </html>
-    );
+      </head>
+      <body>
+        <div id="web-mobile-container" style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'flex-start',
+          width: '100%',
+          minHeight: '100vh',
+          backgroundColor: '#f3f4f6'
+        }}>
+          <div style={{
+            width: '100%',
+            maxWidth: '480px',
+            backgroundColor: '#ffffff',
+            minHeight: '100vh',
+            boxShadow: '0 0 20px rgba(0,0,0,0.05)',
+            position: 'relative',
+            overflowX: 'hidden'
+          }}>
+            {children}
+          </div>
+        </div>
+      </body>
+    </html>
+  );
 }
